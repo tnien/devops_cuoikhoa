fe4fdc41884b3f552b1d32cab538cf9811f21772
Nien <nguyentnien7203@gmail.com>
2026-06-27 03:15:34 +0700

    Add ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,14 @@ resource "aws_instance" "mern_server" {
     "sg-0b1825d46832309e1"
   ]
 
-  tags = {
+
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+    }
+
+    tags = {
     Name = "mern-devops-server"
   }
 }
