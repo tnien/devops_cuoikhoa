944ce1d6bfc4b4c9dea5baa4025db3cdc25daf25
Nien <nguyentnien7203@gmail.com>
2026-06-26 22:01:17 +0700

    Add GitHub Actions CI

--- frontend/src/components/createTask/CreateTask.jsx
+++ frontend/src/components/createTask/CreateTask.jsx
@@ -13,7 +13,7 @@ function CreateTask() {
     const handleAdd = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("/task/addTask", {title, description},{
+             await axios.post("/task/addTask", {title, description},{
               headers: {
                 Authorization: `Bearer ${userToken}`
               }
